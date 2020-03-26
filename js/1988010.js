fbf488e30c0b89f037a44fe33130426e0239ab4e
Nguyen Hai Dang <mr.pi.cbg@gmail.com>
2020-03-26 22:34:08 +0700

    part 4

---
+++ js/1988010.js
@@ -0,0 +1,15 @@
+function loadData(request, targetId, sourceId) {
+    $.ajax({
+        url: 'http://web1-api.herokuapp.com/api/' + request,
+        cache: false,
+        success: function(data) {
+            let jsonData = {
+                data: data
+            };
+            let target = $(targetId);
+            let source = $(sourceId).html();
+            let template = Handlebars.compile(source);
+            $(target).html(template(jsonData));
+        }
+    });
+}
